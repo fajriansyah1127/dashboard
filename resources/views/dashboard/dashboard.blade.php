942965545f783464b370866b3b9649cb885d77c1
fajriansyah1127 <fajriansyah573@gmail.com>
2024-02-15 15:14:30 +0800

    asd

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -37,10 +37,10 @@
         <!-- /top tiles -->
 
         <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="col-md-4 col-sm-4 col-xs-12">
                 <div class="x_panel">
                     <div class="x_title">
-                        <h2>Bar Graph <small>Sessions</small></h2>
+                        <h2>Performansi Project Solution <small>Sessions</small></h2>
                         <ul class="nav navbar-right panel_toolbox">
                             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                             </li>
@@ -64,87 +64,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
-
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="dashboard_graph">
-
-                    <div class="row x_title">
-                        <div class="col-md-6">
-                            <h3>Network Activities <small>Graph title sub-title</small></h3>
-                        </div>
-                        <div class="col-md-6">
-                            <div id="reportrange" class="pull-right"
-                                style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
-                                <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
-                                <span>December 30, 2014 - January 28, 2015</span> <b class="caret"></b>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-9 col-sm-9 col-xs-12">
-                        <div id="chart_plot_01" class="demo-placeholder"></div>
-                    </div>
-                    <div class="col-md-3 col-sm-3 col-xs-12 bg-white">
-                        <div class="x_title">
-                            <h2>Top Campaign Performance</h2>
-                            <div class="clearfix"></div>
-                        </div>
-
-                        <div class="col-md-12 col-sm-12 col-xs-6">
-                            <div>
-                                <p>Facebook Campaign</p>
-                                <div class="">
-                                    <div class="progress progress_sm" style="width: 76%;">
-                                        <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="80">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <p>Twitter Campaign</p>
-                                <div class="">
-                                    <div class="progress progress_sm" style="width: 76%;">
-                                        <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="60">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-12 col-sm-12 col-xs-6">
-                            <div>
-                                <p>Conventional Media</p>
-                                <div class="">
-                                    <div class="progress progress_sm" style="width: 76%;">
-                                        <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="40">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <p>Bill boards</p>
-                                <div class="">
-                                    <div class="progress progress_sm" style="width: 76%;">
-                                        <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="50">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-
-        </div>
-        <br />
-
-        <div class="row">
-
-
             <div class="col-md-4 col-sm-4 col-xs-12">
                 <div class="x_panel tile fixed_height_320">
                     <div class="x_title">
@@ -342,6 +261,89 @@
                 </div>
             </div>
 
+            
+        </div>
+        <div class="row">
+
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="dashboard_graph">
+
+                    <div class="row x_title">
+                        <div class="col-md-6">
+                            <h3>Network Activities <small>Graph title sub-title</small></h3>
+                        </div>
+                        <div class="col-md-6">
+                            <div id="reportrange" class="pull-right"
+                                style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                                <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                                <span>December 30, 2014 - January 28, 2015</span> <b class="caret"></b>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        <div id="chart_plot_01" class="demo-placeholder"></div>
+                    </div>
+                    <div class="col-md-3 col-sm-3 col-xs-12 bg-white">
+                        <div class="x_title">
+                            <h2>Top Campaign Performance</h2>
+                            <div class="clearfix"></div>
+                        </div>
+
+                        <div class="col-md-12 col-sm-12 col-xs-6">
+                            <div>
+                                <p>Facebook Campaign</p>
+                                <div class="">
+                                    <div class="progress progress_sm" style="width: 76%;">
+                                        <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="80">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <p>Twitter Campaign</p>
+                                <div class="">
+                                    <div class="progress progress_sm" style="width: 76%;">
+                                        <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="60">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 col-sm-12 col-xs-6">
+                            <div>
+                                <p>Conventional Media</p>
+                                <div class="">
+                                    <div class="progress progress_sm" style="width: 76%;">
+                                        <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="40">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <p>Bill boards</p>
+                                <div class="">
+                                    <div class="progress progress_sm" style="width: 76%;">
+                                        <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="50">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+
+        </div>
+        <br />
+
+        <div class="row">
+
+
+           
 
             <div class="col-md-4 col-sm-4 col-xs-12">
                 <div class="x_panel tile fixed_height_320">
